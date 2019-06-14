3828e8a5528c79ea8879f405ef07281eab71051a
TMax66 <massimo.tranquillo@gmail.com>
2019-06-14 15:03:56 +0200

    update

---
+++ code.R
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################################################
+############PUBMED MINER################################
+########################################################
+
+library(pubmed.mineR)
+
+abs<-xmlreadabs("pubmed_result.xml")
+abs<-readabs("pubmed_result.txt")
+abs<-cleanabs(abs)
+wa<-word_atomizations(abs) 
+
+
+library(adjutant)
