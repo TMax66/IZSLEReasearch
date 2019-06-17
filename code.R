f7529f6e698b77cf4ec475320c0db0ed2dec1309
TMax66 <massimo.tranquillo@gmail.com>
2019-06-17 15:53:16 +0200

    update

--- code.R
+++ code.R
@@ -1,15 +1,27 @@
+######BIBLIOMETRIX##############
+###############################
+library(tidyverse)
+library(bibliometrix)
+
+
+D <- readFiles("izsler.bib")
+M <- convert2df(D, dbsource = "scopus", format = "bibtex")
+
+results <- biblioAnalysis(M, sep = ";")
+
+NetMatrix <- biblioNetwork(M, analysis = "co-citation", network = "references", sep = ".  ")
 
 
 
+plot(results)
+
+topSO=sourceGrowth(d, top=10, cdf=TRUE)
 
 
-
-
-
-
-
-
-
+library(reshape2)
+library(ggplot2)
+DF=melt(topSO, id='Year')
+ggplot(DF,aes(Year,value, group=variable, color=variable))+geom_line()
 
 
 ########################################################
