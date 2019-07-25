bcd4295e9b53c5f284ef7f42857feecde1d442f1
TMax66 <massimo.tranquillo@gmail.com>
2019-07-25 14:54:28 +0200

    update

--- code.R
+++ code.R
@@ -10,21 +10,93 @@ library(factoextra)
 library(scholar)
 
 rm(list=ls())
-D <- readFiles("izsler.bib")
-M <- convert2df(D, dbsource = "scopus", format = "bibtex")
+izsler<- readFiles("izsler.bib")
+izsler <- convert2df(izsler, dbsource = "scopus", format = "bibtex")
+
+izsve<- readFiles("izsve.bib")
+izsve <- convert2df(izsve, dbsource = "scopus", format = "bibtex")
+
+izsam<- readFiles("izsam.bib")
+izsam <- convert2df(izsam, dbsource = "scopus", format = "bibtex")
+
+####produttività###
+
+A=izsler %>% 
+  group_by(PY) %>% 
+  summarise(n=n()) %>% 
+  filter(PY<2019 & PY>=2005)
+A$Istituto<-rep("izsler", dim(A)[1]) 
+
+B=izsve %>% 
+  group_by(PY) %>% 
+  summarise(n=n()) %>% 
+  filter(PY<2019 & PY>=2005)
+B$Istituto<-rep("izsve", dim(B)[1])
+
+C=izsam %>% 
+  group_by(PY) %>% 
+  summarise(n=n()) %>% 
+  filter(PY<2019 & PY>=2005) 
+
+C$Istituto<-rep("izsam", dim(C)[1])
+
+prod<-rbind(A,B,C)
+
+prod %>% 
+  ggplot(aes(x=PY, y=n, group=Istituto, color=Istituto))+geom_line()+
+  labs(y="n.articoli", x="anno")+
+  scale_x_continuous(breaks=c(2005:2018))
 
 
-M %>% 
+##################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+izsler %>% 
   filter(PY>2005) %>% 
   group_by(SO) %>% 
   summarise(n=n()) %>% 
-  arrange(n) %>% 
   top_n(10, n) %>% 
+  arrange(n) %>% 
+  mutate(SO = factor(SO, unique(SO))) %>% 
+  ggplot(aes(x=SO, y=n))+geom_bar(stat = "identity")+coord_flip()
+
+izsve %>% 
+  filter(PY>2005) %>% 
+  group_by(SO) %>% 
+  summarise(n=n()) %>% 
+  top_n(10, n) %>% 
+  arrange(n) %>% 
+  mutate(SO = factor(SO, unique(SO))) %>% 
+  ggplot(aes(x=SO, y=n))+geom_bar(stat = "identity")+coord_flip()
+
+izsam %>% 
+  filter(PY>2005) %>% 
+  group_by(SO) %>% 
+  summarise(n=n()) %>% 
+  top_n(10, n) %>% 
+  arrange(n) %>% 
   mutate(SO = factor(SO, unique(SO))) %>% 
   ggplot(aes(x=SO, y=n))+geom_bar(stat = "identity")+coord_flip()
 
 
-j<-M %>% 
+
+
+
+j<- %>% 
   filter(PY>2005) %>% 
   group_by(SO) %>% 
   summarise(n=n()) %>% 
@@ -39,11 +111,6 @@ JIF %>%
   ggplot(aes(x=Journal, y=ImpactFactor))+geom_bar(stat = "identity")+coord_flip()
 
 
-M %>% 
-  group_by(PY) %>% 
-  summarise(n=n()) %>% 
-  filter(PY<2019 & PY>2005) %>% 
-  ggplot(aes(PY, y=n))+geom_line()+geom_smooth(method = lm)
 
 
 ######################################
