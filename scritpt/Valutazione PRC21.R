8d51963fdef9786bc550802ca2c71d204ee63e80
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2021-09-28 16:01:06 +0200

    update

--- scritpt/Valutazione PRC21.R
+++ scritpt/Valutazione PRC21.R
@@ -21,34 +21,54 @@ abstrEst %>%
     abstrInt %>% 
       pivot_longer(cols = 7:11, names_to = "area", values_to = "score")
   ) %>% 
-  mutate(approvazione = ifelse(`Vecchia numerazione` %in% deletedpr, "Non Approvati dal Ministero", "Approvati dal Ministero")) %>% View() 
-  # filter(approvazione == "NO") %>% 
-  group_by(Progetto,approvazione) %>% 
-  summarise(Score = sum(score, na.rm = TRUE)) %>% 
+  mutate(approvazione = ifelse(`Vecchia numerazione` %in% deletedpr, "Non Approvati dal Ministero", 
+                               "Approvati dal Ministero")) %>%  
+  filter(approvazione == "Non Approvati dal Ministero") %>% 
+  group_by(Progetto) %>% 
+  summarise(MScore = round(mean(score, na.rm = TRUE),2), 
+            Score = sum(score, na.rm = TRUE) )%>% 
+  pivot_longer(cols = 2:3, names_to = "Valutazione", values_to = "Punteggio") %>% 
+  filter(Valutazione == "Score") %>%  
   mutate(Progetto = factor(Progetto)) %>% 
-  ggplot(aes(x = fct_reorder(Progetto, Score), y = Score, label = Score))+
-  geom_point(size = 8.5, col= "gray", alpha = 0.8)+
-  geom_text(size = 3)+
-  geom_segment(aes(y = 0, x = Progetto, yend = Score-1, xend = Progetto))+ 
+  ggplot(aes(x = fct_reorder(Progetto, Punteggio), y = Punteggio, label = Punteggio))+
+  geom_point(size = 9.5, col= "gray", alpha = 0.8)+
+  geom_text(size = 4)+
+  geom_segment(aes(y = 0, x = Progetto, yend = Punteggio-1, xend = Progetto))+ 
   coord_flip()+ 
   theme_ipsum(axis_title_size = 15)+
-  theme_minimal() +
-  facet_wrap(~approvazione, scale="free")
+  theme_minimal() + labs(title = "Progetti non approvati dal Ministero", y = "Somma punteggi", x = "Codice Progetto")
+  
 
-  abstrEst %>% 
-    pivot_longer(cols = 8:11, names_to = "area", values_to = "score")  %>%
-    select(-7) %>% 
-    bind_rows(
-      abstrInt %>% 
-        pivot_longer(cols = 7:11, names_to = "area", values_to = "score")
-    ) %>% 
-    mutate(approvazione = ifelse(`Vecchia numerazione` %in% deletedpr, "Non Approvati dal Ministero", "Approvati dal Ministero"), 
-           Referee = ifelse(referee %in% c(1,2,3), "Esterni", "Interni"))  %>% 
-    group_by(Progetto, Referee) %>% 
-    summarise(#Score = sum(score, na.rm = TRUE), 
-              MScore = mean(score,  na.rm = TRUE)) %>%
-    pivot_wider(names_from = "Referee", values_from = "MScore") %>%   
-    ggplot(aes(x=Esterni, y=Interni, label = Progetto ))+
-    geom_point(size = 8.5, col="gray50")+
-    geom_text()
+dt <- abstrEst %>% 
+  pivot_longer(cols = 8:11, names_to = "area", values_to = "score")  %>%
+  select(-7) %>% 
+  bind_rows(
+    abstrInt %>% 
+      pivot_longer(cols = 7:11, names_to = "area", values_to = "score")
+  ) %>% View()
+   
+
+
+
+
+
+#facet_wrap(Valutazione~approvazione, scale="free")
+
+  
+# abstrEst %>% 
+#     pivot_longer(cols = 8:11, names_to = "area", values_to = "score")  %>%
+#     select(-7) %>% 
+#     bind_rows(
+#       abstrInt %>% 
+#         pivot_longer(cols = 7:11, names_to = "area", values_to = "score")
+#     ) %>% 
+#     mutate(approvazione = ifelse(`Vecchia numerazione` %in% deletedpr, "Non Approvati dal Ministero", "Approvati dal Ministero"), 
+#            Referee = ifelse(referee %in% c(1,2,3), "Esterni", "Interni"))  %>% 
+#     group_by(Progetto, Referee) %>% 
+#     summarise(#Score = sum(score, na.rm = TRUE), 
+#               MScore = mean(score,  na.rm = TRUE)) %>%
+#     pivot_wider(names_from = "Referee", values_from = "MScore") %>%   
+#     ggplot(aes(x=Esterni, y=Interni, label = Progetto ))+
+#     geom_point(size = 8.5, col="gray50")+
+#     geom_text()
 
