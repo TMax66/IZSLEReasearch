a4e6d19aec82b379f5b20e29b1d75afa28dc3cd9
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2021-10-19 16:31:52 +0200

    update

--- scritpt/Valutazione PRC21.R
+++ scritpt/Valutazione PRC21.R
@@ -12,6 +12,9 @@ finanziati <- c(15,	8,	10,	4,	17,	5,	18,	12,	19,	13,	14,	11,	1,2  )
 deletedpr <- c(3,5, 6, 7, 9, 11, 13, 19, 26,29)
 
 
+ 
+
+
 abstrEst %>% 
   pivot_longer(cols = 7:10, names_to = "area", values_to = "score")  %>% 
   
@@ -28,7 +31,11 @@ group_by(Progetto, Classificazione) %>%
             Score = sum(score, na.rm = TRUE) )%>%   
  # pivot_longer(cols = 2:3, names_to = "Valutazione", values_to = "Punteggio") %>%  
  # filter(Valutazione == "Score") %>%   
-  mutate(Progetto = factor(Progetto)) %>% 
+  mutate(Progetto = factor(Progetto), 
+         Progetto = recode(Progetto, "15" = "Filipello",
+                           "8" = "Villa", 
+                           "10" = "Lorenzi")) %>% View()
+  
   ggplot(aes(x = fct_reorder(Progetto, Score), y = Score, label = Score))+
   geom_point(size = 9.5,  aes(colour = Classificazione), alpha = 0.8)+
   scale_colour_manual(values = c("steelblue","lightblue", "grey"))+
@@ -92,219 +99,219 @@ abstrEst %>%
 
 
 
-abstrInt %>% 
-  select(1, 7:11) %>% 
-  mutate_all(factor) %>%
-  pivot_longer(cols = 2:6, names_to = "Item", values_to = "Freq") %>% 
-  group_by(Item, Freq) %>% 
-  count() %>%  
-  summarise(S = n/29) %>% 
-  mutate(Freq = factor(Freq, levels =c("3", "3.5", "4", "4.5", "5"))) %>% 
-  ggplot(aes(x=Freq, y=S))+
-  geom_bar(stat = "identity")+
-  facet_wrap(~Item)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# abstrInt <- abstrInt %>% 
-#   rename("Chiarezza della proposta" = "Chiarezza obiettivi", 
-#          "Fattibilità" = "Congruità economica") %>% 
-#   select(-7)
-
-
-
-
-# dt <- abstrEst %>% 
-#     pivot_longer(cols = 7:10, names_to = "area", values_to = "score")  %>%
-#     bind_rows(
-#       abstrInt %>%
-#         pivot_longer(cols = 7:11, names_to = "area", values_to = "score")
-#     )
-
-
-
-# library(GGally)
-x <- abstrEst[, 7:10]
-
-x <- x %>%
-  mutate_if(is.numeric, factor)
-library(GDAtools)
-ggpairs(x,
-        axisLabels = "internal",
-        showStrips = TRUE,
-        upper = list( discrete = ggassoc_crosstab))
-
-
-dt <- abstrEst %>%
-  pivot_longer(cols = 7:10, names_to = "area", values_to = "score") %>%
-  mutate(score2 = ifelse(score == 1, "scarso",
-    ifelse(score == 2, "sufficiente",
-      ifelse(score == 3, "buono",
-        ifelse(score == 4, "molto buono", "eccellente")
-      )
-    )
-  )) %>%
-  mutate(score2 = factor(score2, levels = c("scarso", "sufficiente", "buono", "molto buono", "eccellente")))
-
-#MCA----
-library("FactoMineR")
-library("factoextra")
-
-# dt <- abstrEst %>% 
-#   mutate(across(7:10, ~recode(., "1" =  "scarso", 
-#                              "2" =  "sufficiente", 
-#                              "3" = "buono", 
-#                              "4" = "molto buono", 
-#                              "5" = "eccellente")),  
-#          Progetto = as.character(Progetto), 
-#          referee = as.character(referee)) %>% 
-#   select(1, 6:10)
-
-
-
-# x <- MCA(dt[, -c(2)] , quali.sup = 1,   graph = FALSE)
-# fviz_mca_var(x, choice = "mca.cor", 
-#              repel = TRUE, # Avoid text overlapping (slow)
-#              ggtheme = theme_minimal())
-# 
-# var <- get_mca_var(x)
-# head(var$cos2, 4)
-# 
-# fviz_cos2(x, choice = "var")
-# fviz_mca_var(x, 
-#              repel = TRUE, # Avoid text overlapping (slow)
-#              ggtheme = theme_minimal(), m)
+# abstrInt %>% 
+#   select(1, 7:11) %>% 
+#   mutate_all(factor) %>%
+#   pivot_longer(cols = 2:6, names_to = "Item", values_to = "Freq") %>% 
+#   group_by(Item, Freq) %>% 
+#   count() %>%  
+#   summarise(S = n/29) %>% 
+#   mutate(Freq = factor(Freq, levels =c("3", "3.5", "4", "4.5", "5"))) %>% 
+#   ggplot(aes(x=Freq, y=S))+
+#   geom_bar(stat = "identity")+
+#   facet_wrap(~Item)
 # 
 # 
 # 
-# fviz_mca_var(x, col.var = "cos2",
-#              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), 
-#              repel = TRUE, # Avoid text overlapping
-#              ggtheme = theme_minimal())
 # 
 # 
-# fviz_mca_var(x, col.var = "contrib",
-#              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07", "#FC0000"), 
-#              repel = TRUE, # avoid text overlapping (slow)
-#              ggtheme = theme_minimal())
-# 
-# fviz_mca_biplot(x, repel = TRUE,
-#                 ggtheme = theme_minimal())
 # 
 # 
-# fviz_mca_ind(x, col.ind = "cos2", 
-#              #gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-#              repel = TRUE, # Avoid text overlapping (slow if many points)
-#              ggtheme = theme_minimal())
 # 
-
-
- 
- 
-   
-   
-   
-     
-     
-   
-  
- 
-
-
-
-
-
-
-dt %>%
-  group_by(area, score2, Progetto) %>% 
-  summarise(n= n()) %>% 
-  ggplot(aes(score2,area , label = n)) +
-  geom_tile( fill = "white")+
-  facet_wrap(~Progetto)+
-  geom_text()+
-  theme_ipsum_rc()
-  
-
-
-
-
-  # #filter(area == "Impatto" ) %>%
-  # ggplot(aes(x=score2))+
-  #    facet
-  # 
-  # geom_bar()+  coord_flip()+ labs(x = "")+
-  # facet_grid(area~Progetto)
-
-
-
-x <- LETTERS[1:20]
-y <- paste0("var", seq(1,20))
-data <- expand.grid(X=x, Y=y)
-data$Z <- runif(400, 0, 5)
-
-ggplot(data, aes(X, Y, fill= Z)) + 
-  geom_tile()
-
 # 
-# ggpairs(tips[, 3:6], 
+# 
+# 
+# 
+# 
+# 
+# 
+# # abstrInt <- abstrInt %>% 
+# #   rename("Chiarezza della proposta" = "Chiarezza obiettivi", 
+# #          "Fattibilità" = "Congruità economica") %>% 
+# #   select(-7)
+# 
+# 
+# 
+# 
+# # dt <- abstrEst %>% 
+# #     pivot_longer(cols = 7:10, names_to = "area", values_to = "score")  %>%
+# #     bind_rows(
+# #       abstrInt %>%
+# #         pivot_longer(cols = 7:11, names_to = "area", values_to = "score")
+# #     )
+# 
+# 
+# 
+# # library(GGally)
+# x <- abstrEst[, 7:10]
+# 
+# x <- x %>%
+#   mutate_if(is.numeric, factor)
+# library(GDAtools)
+# ggpairs(x,
 #         axisLabels = "internal",
-#         showStrips = TRUE)
+#         showStrips = TRUE,
+#         upper = list( discrete = ggassoc_crosstab))
+# 
+# 
+# dt <- abstrEst %>%
+#   pivot_longer(cols = 7:10, names_to = "area", values_to = "score") %>%
+#   mutate(score2 = ifelse(score == 1, "scarso",
+#     ifelse(score == 2, "sufficiente",
+#       ifelse(score == 3, "buono",
+#         ifelse(score == 4, "molto buono", "eccellente")
+#       )
+#     )
+#   )) %>%
+#   mutate(score2 = factor(score2, levels = c("scarso", "sufficiente", "buono", "molto buono", "eccellente")))
+# 
+# #MCA----
+# library("FactoMineR")
+# library("factoextra")
+# 
+# # dt <- abstrEst %>% 
+# #   mutate(across(7:10, ~recode(., "1" =  "scarso", 
+# #                              "2" =  "sufficiente", 
+# #                              "3" = "buono", 
+# #                              "4" = "molto buono", 
+# #                              "5" = "eccellente")),  
+# #          Progetto = as.character(Progetto), 
+# #          referee = as.character(referee)) %>% 
+# #   select(1, 6:10)
 # 
 # 
 # 
-# library("gplots")
+# # x <- MCA(dt[, -c(2)] , quali.sup = 1,   graph = FALSE)
+# # fviz_mca_var(x, choice = "mca.cor", 
+# #              repel = TRUE, # Avoid text overlapping (slow)
+# #              ggtheme = theme_minimal())
+# # 
+# # var <- get_mca_var(x)
+# # head(var$cos2, 4)
+# # 
+# # fviz_cos2(x, choice = "var")
+# # fviz_mca_var(x, 
+# #              repel = TRUE, # Avoid text overlapping (slow)
+# #              ggtheme = theme_minimal(), m)
+# # 
+# # 
+# # 
+# # fviz_mca_var(x, col.var = "cos2",
+# #              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), 
+# #              repel = TRUE, # Avoid text overlapping
+# #              ggtheme = theme_minimal())
+# # 
+# # 
+# # fviz_mca_var(x, col.var = "contrib",
+# #              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07", "#FC0000"), 
+# #              repel = TRUE, # avoid text overlapping (slow)
+# #              ggtheme = theme_minimal())
+# # 
+# # fviz_mca_biplot(x, repel = TRUE,
+# #                 ggtheme = theme_minimal())
+# # 
+# # 
+# # fviz_mca_ind(x, col.ind = "cos2", 
+# #              #gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+# #              repel = TRUE, # Avoid text overlapping (slow if many points)
+# #              ggtheme = theme_minimal())
+# # 
 # 
-# library(fastDummies)
 # 
-# dummydt <- abstrEst %>%
-#   select(7:10) %>%
-#   dummy_cols(remove_selected_columns = TRUE)
-# 
-# 
-# 
-# df<-data.frame(dt[, 9],dummydt)
-# 
-# 
-# 
-# 
-# 
-# 
-# tabella <-  df %>%
-#   group_by(ageclass) %>%
-#   summarise_all(sum, na.rm = T)  %>%
-#   select(ageclass, ends_with("P")) %>% View()
-#   column_to_rownames(var="ageclass") %>%
-#   as.data.frame() %>%
-#   select(1:5)
+#  
+#  
+#    
+#    
+#    
+#      
+#      
+#    
+#   
+#  
 # 
 # 
 # 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 
+# 
+# 
+# dt %>%
+#   group_by(area, score2, Progetto) %>% 
+#   summarise(n= n()) %>% 
+#   ggplot(aes(score2,area , label = n)) +
+#   geom_tile( fill = "white")+
+#   facet_wrap(~Progetto)+
+#   geom_text()+
+#   theme_ipsum_rc()
+#   
+# 
+# 
+# 
+# 
+#   # #filter(area == "Impatto" ) %>%
+#   # ggplot(aes(x=score2))+
+#   #    facet
+#   # 
+#   # geom_bar()+  coord_flip()+ labs(x = "")+
+#   # facet_grid(area~Progetto)
+# 
+# 
+# 
+# x <- LETTERS[1:20]
+# y <- paste0("var", seq(1,20))
+# data <- expand.grid(X=x, Y=y)
+# data$Z <- runif(400, 0, 5)
+# 
+# ggplot(data, aes(X, Y, fill= Z)) + 
+#   geom_tile()
+# 
+# # 
+# # ggpairs(tips[, 3:6], 
+# #         axisLabels = "internal",
+# #         showStrips = TRUE)
+# # 
+# # 
+# # 
+# # library("gplots")
+# # 
+# # library(fastDummies)
+# # 
+# # dummydt <- abstrEst %>%
+# #   select(7:10) %>%
+# #   dummy_cols(remove_selected_columns = TRUE)
+# # 
+# # 
+# # 
+# # df<-data.frame(dt[, 9],dummydt)
+# # 
+# # 
+# # 
+# # 
+# # 
+# # 
+# # tabella <-  df %>%
+# #   group_by(ageclass) %>%
+# #   summarise_all(sum, na.rm = T)  %>%
+# #   select(ageclass, ends_with("P")) %>% View()
+# #   column_to_rownames(var="ageclass") %>%
+# #   as.data.frame() %>%
+# #   select(1:5)
+# # 
+# # 
+# # 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
 
 # abstrInt %>% 
 #   select(-`Attinenza linee ministeriali`) %>% 
